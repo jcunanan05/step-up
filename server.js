ddb5c78020243c114d3b4565653effbaa0cbe59b
jcunanan05 <j_cunanan05@yahoo.com>
2018-09-22 01:30:20 -0400

    Remove broken css link in <head>, Edit lint-staged

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const next = require('next');
-
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -13,9 +13,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`);
   })
 })
 .catch((ex) => {
--- src/components/__head.js
+++ src/components/__head.js
@@ -6,6 +6,5 @@ export default () => (
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="/_next/static/style.css" />
   </Head>
 );
