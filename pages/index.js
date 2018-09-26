ce1bbe4b1f2572bd7bee4b1611a61386982b1c8a
jcunanan05 <j_cunanan05@yahoo.com>
2018-09-26 14:39:07 -0400

    Fix index page structure
    Made pages/index.js a component class because of the render hero and navbar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Component, Fragment } from 'react';
 import HomeTemplate from '../src/templates/HomeTemplate';
 import Columns, { Column } from '../src/components/Columns';
 import Section from '../src/components/Section';
@@ -8,74 +8,79 @@ import Hero from '../src/components/Hero';
 
 import '../src/styles/pages/_index.scss';
 
-const renderNavbar = (
-  <Navbar
-    brand={<Item text="StepUP" />}
-    right={
-      <Fragment>
-        <Item text="Who We Are" />
-        <Item text="Upcoming Projects" />
-        <Item text="News" />
-      </Fragment>
-    }
-    onMenuClick={event => {
-      event.preventDefault();
-      const section = document.querySelector(event.target.getAttribute('href'));
-      if (section)
-        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }}
-  />
-);
+export default class Index extends Component {
+  onMenuClick(event) {
+    event.preventDefault();
+    const section = document.querySelector(event.target.getAttribute('href'));
+    if (section) section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 
-const renderHero = (
-  <Hero
-    className="is-dark is-fullheight home__banner"
-    head={renderNavbar}
-    body={
-      <header className="container has-text-centered">
-        <h1 className="title is-1">StepUP!</h1>
-        <h2 className="subtitle">Community Initiative</h2>
-      </header>
-    }
-  />
-);
+  renderHero = () => (
+    <Hero
+      className="is-dark is-fullheight home__banner"
+      head={this.renderNavbar()}
+      body={
+        <header className="container has-text-centered">
+          <h1 className="title is-1">StepUP!</h1>
+          <h2 className="subtitle">Community Initiative</h2>
+        </header>
+      }
+    />
+  );
 
-export default () => (
-  <HomeTemplate banner={renderHero}>
-    <Section name="who-we-are" className="content">
-      <Columns>
-        <Column className="is-one-third">
-          <h2 className="title is-2">Who We Are</h2>
-        </Column>
-        <Column>
+  renderNavbar = () => (
+    <Navbar
+      brand={<Item text="StepUP" />}
+      right={
+        <Fragment>
+          <Item text="Who We Are" />
+          <Item text="Upcoming Projects" />
+          <Item text="News" />
+        </Fragment>
+      }
+      onMenuClick={this.onMenuClick}
+    />
+  );
+
+  render() {
+    return (
+      <HomeTemplate banner={this.renderHero()}>
+        <Section name="who-we-are" className="content">
+          <Columns>
+            <Column className="is-one-third">
+              <h2 className="title is-2">Who We Are</h2>
+            </Column>
+            <Column>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                placeat vel repellendus voluptates temporibus assumenda dolore
+                unde! Tempore, inventore perspiciatis quia fugit cumque, sed
+                pariatur sunt repellendus voluptatibus placeat commodi?
+              </p>
+            </Column>
+          </Columns>
+        </Section>
+
+        <Section name="upcoming-projects" className="content">
+          <h2 className="title is-2">Upcoming Projects</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
             placeat vel repellendus voluptates temporibus assumenda dolore unde!
             Tempore, inventore perspiciatis quia fugit cumque, sed pariatur sunt
             repellendus voluptatibus placeat commodi?
           </p>
-        </Column>
-      </Columns>
-    </Section>
+        </Section>
 
-    <Section name="upcoming-projects" className="content">
-      <h2 className="title is-2">Upcoming Projects</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias placeat
-        vel repellendus voluptates temporibus assumenda dolore unde! Tempore,
-        inventore perspiciatis quia fugit cumque, sed pariatur sunt repellendus
-        voluptatibus placeat commodi?
-      </p>
-    </Section>
-
-    <Section name="news" className="content">
-      <h2 className="title is-2">News</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias placeat
-        vel repellendus voluptates temporibus assumenda dolore unde! Tempore,
-        inventore perspiciatis quia fugit cumque, sed pariatur sunt repellendus
-        voluptatibus placeat commodi?
-      </p>
-    </Section>
-  </HomeTemplate>
-);
+        <Section name="news" className="content">
+          <h2 className="title is-2">News</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            placeat vel repellendus voluptates temporibus assumenda dolore unde!
+            Tempore, inventore perspiciatis quia fugit cumque, sed pariatur sunt
+            repellendus voluptatibus placeat commodi?
+          </p>
+        </Section>
+      </HomeTemplate>
+    );
+  }
+}
