33c543f43f809f304398622b14b05ab3db3345d0
jcunanan05 <j_cunanan05@yahoo.com>
2018-10-24 16:17:53 -0400

    Add Google Verification

--- src/views/components/__head.js
+++ src/views/components/__head.js
@@ -7,6 +7,13 @@ const defaultData = {
     'Step Up Now Community aims to help youth and newcomers. Contact us now!'
 };
 
+const GoogleVerifcation = () => (
+  <meta
+    name="google-site-verification"
+    content="rvpV57YcjGmMsLLd5r2krZaf1k99b_45xqgP5fD2PdU"
+  />
+);
+
 const ModifiedHead = ({
   title = defaultData.title,
   description = defaultData.description
@@ -17,6 +24,7 @@ const ModifiedHead = ({
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
     <meta name="description" content={description} />
+    <GoogleVerifcation />
   </Head>
 );
 
